235ec7ae3370dcb672c23ad5d45fd1cad8945c07
raymondsquared <raymond_squared@live.com>
2018-05-19 21:13:28 +1000

    Parse the dataset

--- eager-execution/eager-execution.py
+++ eager-execution/eager-execution.py
@@ -42,3 +42,14 @@ train_dataset_fp = tf.keras.utils.get_file(
     origin=train_dataset_url)
 
 print("Local copy of the dataset file: {}".format(train_dataset_fp))
+
+
+# Parse the dataset
+def parse_csv(line):
+    example_defaults = [[0.], [0.], [0.], [0.], [0]]  # sets field types
+    parsed_line = tf.decode_csv(line, example_defaults)
+    # First 4 fields are features, combine into single tensor
+    features = tf.reshape(parsed_line[:-1], shape=(4,))
+    # Last field is the label
+    label = tf.reshape(parsed_line[-1], shape=())
+    return features, label
