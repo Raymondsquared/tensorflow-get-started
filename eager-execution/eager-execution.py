23ce2022695768e5e65b8f916cf832dfc8540187
raymondsquared <raymond_squared@live.com>
2018-05-19 21:43:27 +1000

    Setup the test dataset

--- eager-execution/eager-execution.py
+++ eager-execution/eager-execution.py
@@ -165,3 +165,18 @@ axes[1].set_xlabel("Epoch", fontsize=14)
 axes[1].plot(train_accuracy_results)
 
 plt.show()
+
+
+# Setup the test dataset
+test_url = "http://download.tensorflow.org/data/iris_test.csv"
+
+test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
+                                  origin=test_url)
+
+test_dataset = tf.data.TextLineDataset(test_fp)
+test_dataset = test_dataset.skip(1)             # skip header row
+# parse each row with the funcition created earlier
+test_dataset = test_dataset.map(parse_csv)
+test_dataset = test_dataset.shuffle(1000)       # randomize
+# use the same batch size as the training set
+test_dataset = test_dataset.batch(32)
